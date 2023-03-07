250f5e05dba87280ee20460d598f1f56aa3d4a07
WolfWilson <127243660+WolfWilson@users.noreply.github.com>
2023-03-07 15:48:00 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EJERCICIOS-JAVASCRIPT
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-cenrfc)
---
+++ index.js
@@ -0,0 +1,9 @@
+//ejercicio para mostrar tipo de dato de una variable con una funcion
+
+let dato = 28;
+function mostrartipodedato(edad){
+    return typeof edad;
+}
+
+console.log (mostrartipodedato (dato));
+  
